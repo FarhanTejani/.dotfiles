dfddc4b6f7571920d3b157dc6215bc5cf2409d8a
Farhan Tejani <farhantejani@gmail.com>
2016-09-17 23:08:43 -0400

    Created install script for OS X

---
+++ install-osx.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Software I need on every mac system
+
+brew update
+brew upgrade
+
+# Basic command line utilities
+brew install tree
+brew install git
+brew install ngrok
+brew install the_silver_searcher
+brew install tmux
+brew install z
+
+# Programming languages
+brew install gradle
+brew install node
+brew install python
+
+# The best text editor
+brew install vim
+
+# The best shell
+brew install zsh
+
+# Just fun
+brew install cmatrix
+
+# Remove outdated versions from the cellar.
+brew cleanup
+brew linkapps
+
+echo install-osx.sh complete.
