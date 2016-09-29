31dcef90b0e15ec38ba1afefeafe26fa4662a38d
Farhan Tejani <farhantejani@gmail.com>
2016-09-28 20:15:02 -0400

    Properly sets up zsh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
-############################
+##############################
 # .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+# This script creates symlinks from the home directory
+# to any desired dotfiles in ~/.dotfiles and sets up vim and zsh
+##############################
 
 ########## Variables
 
-dir=~/.dotfiles                    # dotfiles directory
-olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="zshrc zshenv tmux.conf vimrc"    # list of files/folders to symlink in homedir
+dir=~/.dotfiles # dotfiles directory
+olddir=~/.dotfiles_old # old dotfiles backup directory
+files="zshrc zshenv tmux.conf vimrc" # list of files to symlink in homedir
 
 ##########
 
-# create dotfiles_old in homedir
+# Install vundle
+echo "##### Installing vundle #####"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install oh-my-zsh
+echo "##### Installing oh-my-zsh #####"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+
+# Create dotfiles_old in homedir
 echo "##### Creating $olddir for backup of any existing dotfiles #####"
 mkdir -p $olddir
 
-# change to the dotfiles directory
-echo "##### Changing to the $dir directory #####"
+# Move any existing dotfiles in homedir to dotfiles_old directory, then create
+# symlinks from the homedir to any files in the ~/.dotfiles directory specified in $files
 cd $dir
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
     echo "##### Moving any existing dotfiles to $olddir #####"
     mv ~/.$file $olddir
@@ -28,13 +35,8 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-# Set up oh-my-zsh
-echo "##### Installing oh-my-zsh #####"
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-# Set up vundle and compile YCM
-echo "##### Installing vim packages #####"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install vundle plugins and compile YCM
+echo "##### Installing vundle packages #####"
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --all
@@ -42,8 +44,7 @@ cd ~/.vim/bundle/YouCompleteMe
 # Set zsh as default shell
 echo "##### Setting zsh as default shell #####"
 echo "##### NOTE: THIS REQUIRES SUDO ACCESS #####"
-sudo -s
-(which zsh) >> /etc/shells
-chsh -s (which zsh)
+echo $(which zsh) | sudo tee -a /etc/shells
+chsh -s $(which zsh)
 
 echo "##### Setup Complete! Please restart the shell #####"
